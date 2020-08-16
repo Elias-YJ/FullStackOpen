8d5dd59423883a87b8cd166b1e07e0a7fc7b3aa5
Elias-YJ <elias.yla-jarkko@live.fi>
2020-08-16 22:01:35 +0300

    4.2 Modularize code

---
+++ Osa 4/blogilista/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true})
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
