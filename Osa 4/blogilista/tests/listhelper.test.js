d221f2d859f09d07860135a3cc19d318bc21de65
Elias-YJ <elias.yla-jarkko@live.fi>
2020-08-16 22:27:29 +0300

    4.3-4.4 Create unit tests

---
+++ Osa 4/blogilista/tests/listhelper.test.js
@@ -0,0 +1,49 @@
+const listHelper = require('../utils/list_helper')
+
+describe('dummy', () => {
+  test('dummy returns one', () => {
+    const blogs = []
+  
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+  })
+})
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithTwoBlogs = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    },
+    {
+      title: "Tarjoilusuositus",
+      author: "teddypunkkari",
+      url: "http://tarjoiluehdotus.blogspot.com/",
+      likes: 6
+  }
+  ]
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  test('when list has two blogs then totalLikes equals the sum of likes', () => {
+    const result = listHelper.totalLikes(listWithTwoBlogs)
+    expect(result).toBe(11)
+  })
+})
