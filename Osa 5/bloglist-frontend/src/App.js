ba8e2687c4c0cbac0ac99233e4413072beaa4206
Elias-YJ <elias.yla-jarkko@live.fi>
2020-10-04 15:45:15 +0300

    5.1 login works

--- Osa 5/bloglist-frontend/src/App.js
+++ Osa 5/bloglist-frontend/src/App.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Login from './components/Login'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,6 +16,41 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUsername('')
+      setPassword('')
+      setUser(user)
+    } catch (exception) {
+      //setErrorMessage('wrong credentials')
+      //setTimeout(() => {
+      //  setErrorMessage(null)
+      //}, 5000)
+    }
+  }
+
+  const credentials = {
+    username: {
+      text: username, 
+      handler: setUsername
+    },
+    password: {
+      text: password,
+      handler: setPassword
+    }
+  }
+  if (user === null) {
+    return (
+      <div>
+        <h2>Log in to application</h2>
+        <Login credentials={credentials} loginHandler={handleLogin} />
+      </div>
+    )
+  }
   return (
     <div>
       <h2>blogs</h2>
---
+++ Osa 5/bloglist-frontend/src/components/Login.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Username = ({ username, changeHandler }) => (
+  <div>
+    username
+      <input
+      type="text"
+      value={username}
+      name="Username"
+      onChange={({ target }) => changeHandler(target.value)}
+    />
+  </div>
+)
+
+const Password = ({ password, changeHandler }) => (
+  <div>
+    password
+      <input
+      type="password"
+      value={password}
+      name="Password"
+      onChange={({ target }) => changeHandler(target.value)}
+    />
+  </div>
+)
+
+const Login = (({ credentials, loginHandler }) => {
+  return(
+    <form onSubmit={loginHandler}>
+        <Username username={credentials.username.text} changeHandler={credentials.username.handler}/>
+        <Password password={credentials.password.text} changeHandler={credentials.password.handler}/>
+        <button type="submit">login</button>
+    </form>
+  )
+})
+export default Login
