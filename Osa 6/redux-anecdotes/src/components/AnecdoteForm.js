bdfe1a97a49d25bbb2617586ab913dfebdb7bfe9
Elias-YJ <elias.yla-jarkko@live.fi>
2020-10-11 00:50:48 +0300

    6.19-6.20 use connect in notification and form

--- Osa 6/redux-anecdotes/src/components/AnecdoteForm.js
+++ Osa 6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -1,17 +1,16 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 
 const AnecdoteForm = (props) => {
-  const dispatch = useDispatch()
-  
+
   const addAnecdote = async (event) => {
     event.preventDefault()
     const content = event.target.content.value
     event.target.content.value = ''
-    dispatch(createAnecdote(content))
-    dispatch(setNotification(`You added "${content}"`, 5))
+    props.createAnecdote(content)
+    props.setNotification(`You added "${content}"`, 5)
   }
 
   return (
@@ -25,4 +24,9 @@ const AnecdoteForm = (props) => {
   )
 }
 
-export default AnecdoteForm
+const ConnectedAnecdoteForm = connect(
+  null,
+  {createAnecdote, setNotification},
+  )(AnecdoteForm)
+
+export default ConnectedAnecdoteForm
